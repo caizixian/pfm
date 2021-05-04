ba5b2a390ab68110ccf527c1fd8b7ca687bcc5e2
Zixian Cai <u5937495@anu.edu.au>
2021-05-04 16:12:54 +1000

    Build from source and link statically

--- build.rs
+++ build.rs
@@ -2,26 +2,56 @@ extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
+
+const LIBPFM_VERSION: &'static str = "4.11.0";
 
 fn main() {
-    // Tell cargo to tell rustc to link the system libpfm4
-    // shared library.
-    println!("cargo:rustc-link-lib=pfm");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let libpfm_filename = format!("libpfm-{}.tar.gz", LIBPFM_VERSION);
+    let download_url = format!(
+        "https://sourceforge.net/projects/perfmon2/files/libpfm4/{}/download",
+        libpfm_filename
+    );
+    Command::new("wget")
+        .current_dir(&out_path)
+        .arg("-O")
+        .arg(&libpfm_filename)
+        .arg(download_url)
+        .status()
+        .unwrap();
 
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=wrapper.h");
+    Command::new("tar")
+        .current_dir(&out_path)
+        .arg("xf")
+        .arg(&libpfm_filename)
+        .status()
+        .unwrap();
+    let libpfm_dir = out_path.join(format!("libpfm-{}", LIBPFM_VERSION));
+    Command::new("make")
+        .env("CFLAGS", "-fPIC")
+        .current_dir(&libpfm_dir)
+        .status()
+        .unwrap();
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
+    println!(
+        "cargo:rustc-link-search=native={}",
+        &libpfm_dir.join("lib").to_str().unwrap()
+    );
+    println!("cargo:rustc-link-lib=static=pfm");
+
+    let header = &libpfm_dir
+        .join("include")
+        .join("perfmon")
+        .join("pfmlib_perf_event.h");
+    println!("cargo:rerun-if-changed={}", (&header).to_str().unwrap());
+
     let bindings = bindgen::Builder::default()
         .allowlist_type("^(pfm|PFM).*")
         .allowlist_function("^(pfm|PFM).*")
         .allowlist_var("^(pfm|PFM).*")
         .default_macro_constant_type(bindgen::MacroTypeVariation::Signed)
-        // The input header we would like to generate
-        // bindings for.
-        .header("wrapper.h")
+        .header((&header).to_str().unwrap())
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -31,7 +61,6 @@ fn main() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
--- wrapper.h
+++
@@ -1,1 +0,0 @@
-#include <perfmon/pfmlib_perf_event.h>
