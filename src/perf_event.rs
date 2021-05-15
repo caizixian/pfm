1fd1d2b744d5b8bc808abc74d13b71f91d8a823a
Zixian Cai <u5937495@anu.edu.au>
2021-05-16 00:20:12 +1000

    Implement counter read

--- src/perf_event.rs
+++ src/perf_event.rs
@@ -1,5 +1,5 @@
 use super::util::pfm_err_description;
-use libc::c_int;
+use libc::{c_int, c_longlong, c_void, read};
 use perf_event_open_sys::{ioctls, perf_event_open};
 use pfm_sys::{
     perf_event_attr, perf_event_read_format, pfm_get_perf_event_encoding, PFM_PLM3, PFM_SUCCESS,
@@ -10,6 +10,12 @@ use std::mem::MaybeUninit;
 pub struct PerfEvent {
     pe: perf_event_attr,
     fd: Option<c_int>,
+}
+
+pub struct PerfEventValue {
+    pub value: i64,
+    pub time_enabled: i64,
+    pub time_running: i64,
 }
 
 impl PerfEvent {
@@ -67,5 +73,44 @@ impl PerfEvent {
                 ioctls::RESET(fd, 0);
             }
         }
+    }
+
+    pub fn read(&self) -> Option<PerfEventValue> {
+        let mut counts: [c_longlong; 3] = [0; 3];
+        self.fd.map(|fd| {
+            unsafe {
+                read(
+                    fd,
+                    (&mut counts) as *mut _ as *mut c_void,
+                    std::mem::size_of::<[c_longlong; 3]>(),
+                );
+            }
+            PerfEventValue {
+                value: counts[0],
+                time_enabled: counts[1],
+                time_running: counts[2],
+            }
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_retired_instructions() {
+        use crate::Perfmon;
+        let mut perfmon: Perfmon = Default::default();
+        perfmon.initialize().unwrap();
+        let mut event = PerfEvent::new("RETIRED_INSTRUCTIONS").unwrap();
+        event.open().unwrap();
+        event.enable();
+        println!("Measuring instruction count for this println");
+        let counts = event.read().unwrap();
+        println!(
+            "Raw: {}, total time enabled: {}, total time running {}",
+            counts.value, counts.time_enabled, counts.time_running
+        );
+        event.disable();
     }
 }
